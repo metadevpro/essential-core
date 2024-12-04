0accb103ff53b3df714ead6e03522d879812e8e4
Pedro J. Molina <pjmolina@metadev.pro>
2024-12-04 21:07:44 +0100

    improved types

--- src/modelBase.ts
+++ src/modelBase.ts
@@ -1,11 +1,15 @@
 import { IError, Model } from './modelElement';
-import { ModelElementBase } from './modelElementBase';
+import { ModelElementBase, ModelElementBaseBuilder } from './modelElementBase';
+
+export interface ModelBaseBuilder
+  extends ModelElementBaseBuilder,
+    Partial<ModelBase> {}
 
 export class ModelBase extends ModelElementBase implements Model {
   modelName: string;
   version: string = '0';
 
-  constructor(options: Partial<ModelBase> = {}) {
+  constructor(options: ModelBaseBuilder = {}) {
     super(options);
     if (options.modelName) {
       this.modelName = options.modelName;
